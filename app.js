7b8eaf76d30283aacc0d2dd5ba128a6870c13c12
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2022-12-05 21:09:13 -0500

    logic added and almost all functionality done

--- app.js
+++ app.js
@@ -3,52 +3,118 @@
 const Player = (name, mark) => {
     this.name = name
     this.mark = mark
-    Active
-
 }
 
-let gameboardArray = ['', '', '', '', '', '', '', '', '']
+let gameboardArray = Array(9).fill('')
 let turn = 0
+let gameEnded = false
+let firstPlayerTurn = true
 
-const gameboard = (() => {
-    const array = ['O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O']
-
-    const checkWin = ''
-
-
-}
-
-)
 
 // DOM Elements
 
 boxes = document.querySelectorAll('.box')
+restart = document.querySelector('.restart')
+userOne = document.querySelector('.userOne')
+userTwo = document.querySelector('.userTwo')
+winner = document.querySelector('.winner')
+
 
 
 
 //EventListeners
 
-boxes.forEach(box => { box.addEventListener('click', updateBoard)  
+boxes.forEach(box => { box.addEventListener('click', game)  
 });
+restart.addEventListener('click', cleanBoard)
 
 
 //Functions
-let firstPlayerTurn = true
+
+function game(e){
+    activePlayer()
+    updateBoard(e)
+    gameEnded = checkWin(gameboardArray)
+    changeTurn()
+    endGame()
+}
 
 function updateBoard(e) {
     let marker = firstPlayerTurn? 'X' : '0'
-    gameboardArray[e.target.getAttribute('value')] = marker
+    markedIndex = e.target.getAttribute('value')
+    if (gameboardArray[markedIndex] != '') {
+        return 0
+    }
+    gameboardArray[markedIndex] = marker
     boxes.forEach(box => {
-        let index = box.getAttribute('value')
-        box.innerText = gameboardArray[index]
+        let boxIndex = box.getAttribute('value')
+        box.innerText = gameboardArray[boxIndex]
     })
-    firstPlayerTurn = !firstPlayerTurn
 }
 
-function markBoard(){
-    return ''
+function checkWin(array){
+    //*ROWS
+    if (array[0] == array[1] && array[0] == array[2] && array[0] != ''){
+        return true
+    }
+    if (array[3] == array[4] && array[3] == array[5] && array[3] != ''){
+        return true
+    }
+    if (array[6] == array[7] && array[6] == array[8] && array[6] != ''){
+        return true
+    }
+    //*COLUMNS
+    if (array[0] == array[3] && array[0] == array[6] && array[0] != ''){
+        return true
+    }
+    if (array[1] == array[4] && array[1] == array[7] && array[1] != ''){
+        return true
+    }
+    if (array[2] == array[5] && array[2] == array[8] && array[2] != ''){
+        return true
+    }
+    //*DIAGONALS
+    if (array[0] == array[4] && array[0] == array[8] && array[0] != ''){
+        return true
+    }
+    if (array[2] == array[4] && array[2] == array[6] && array[2] != ''){
+        return true
+    }
+    else{
+        return false
+    }
 }
+
+function endGame(){
+    if (turn === 9 || gameEnded) {
+        console.log('game ended')
+        boxes.forEach(box => { box.removeEventListener('click', game)  
+        });
+        if (gameEnded){
+            winner.innerText = `The winner is ${firstPlayerTurn?'Player 2': 'Player 1'}`
+        }
+        else{
+            winner.innerText = `It's a tie`
+        }
+        return
+    }
+    console.log('game is going')
+}
+
+function changeTurn(){
+    firstPlayerTurn = !firstPlayerTurn
+    turn++
+}
+
 
 function cleanBoard(){
-    return ''
+    gameboardArray = Array(9).fill('')
+    turn = 0
+    boxes.forEach(box => box.innerText = '')
+    boxes.forEach(box => box.addEventListener('click', game) )
+}
+
+function activePlayer(){
+    userOne.classList.toggle('playerActive')
+    userTwo.classList.toggle('playerActive')
 }
